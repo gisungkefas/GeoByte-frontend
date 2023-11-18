3e01dcd72c2459a6ce6f9f0e9cdb4bb5baa450f2
Kefas Gisung <kefasg71@gmail.com>
2023-11-18 17:48:06 +0100

    Implemented Login with jwt

--- src/app/_services/user-auth.service.ts
+++ src/app/_services/user-auth.service.ts
@@ -1,22 +1,40 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserAuthService {
-
-  constructor() { }
+  constructor() {}
 
   public setRoles(roles: []) {
     localStorage.setItem('roles', JSON.stringify(roles));
   }
 
+  public getRoles(): string[] {
+    const rolesString = localStorage.getItem('roles');
+
+    if (rolesString) {
+        return JSON.parse(rolesString);
+    } else {
+        return [];
+    }
+}
+
   public setToken(jwtToken: string) {
     localStorage.setItem('jwtToken', jwtToken);
   }
+
+  public getToken(): string | null {
+    const token = localStorage.getItem('jwtToken');
+    return token !== null ? token : null;
+}
+
 
   public clear() {
     localStorage.clear();
   }
 
+  public isLoggedIn() {
+    return this.getRoles() && this.getToken();
+  }
 }
--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,18 +1,60 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UserAuthService } from './user-auth.service';
+
+export interface LoginData {
+  username: string;
+  password: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
+
 export class UserService {
+  PATH_OF_API = 'http://localhost:8888';
 
-  PATH_OF_API =  "http://localhost:8888";
-  requestHeader = new HttpHeaders(
-    { "No-Auth":"True"}
-  );
-  constructor(private httpclient: HttpClient) { }
+  requestHeader = new HttpHeaders({ 'No-Auth': 'True' });
+  constructor(
+    private httpclient: HttpClient,
+    private userAuthService: UserAuthService
+  ) {}
 
-  public login(loginData){
-    return this.httpclient.post(this.PATH_OF_API + "/api/v1/auth/login", loginData, {headers: this.requestHeader})
+  public login(loginData: LoginData) {
+    return this.httpclient.post(this.PATH_OF_API + '/authenticate', loginData, {
+      headers: this.requestHeader,
+    });
   }
+
+  public forUser() {
+    return this.httpclient.get(this.PATH_OF_API + '/forUser', {
+      responseType: 'text',
+    });
+  }
+
+
+  public forAdmin() {
+    return this.httpclient.get(this.PATH_OF_API + '/forAdmin', {
+      responseType: 'text',
+    });
+  }
+
+  public roleMatch(allowedRoles: string[]): boolean {
+    let isMatch = false;
+    const userRoles: string[] | null = this.userAuthService.getRoles();
+  
+    if (userRoles !== null) {
+      for (let i = 0; i < userRoles.length; i++) {
+        for (let j = 0; j < allowedRoles.length; j++) {
+          if (userRoles[i] === allowedRoles[j]) {
+            isMatch = true;
+            return isMatch;
+          }
+        }
+      }
+    }
+    
+    return isMatch;
+  }
+  
 }
